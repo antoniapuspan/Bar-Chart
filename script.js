0df54432fd3f3ce3125ed2e5fab73bd027dfc341
antoniapuspan <65085166+antoniapuspan@users.noreply.github.com>
2021-02-11 23:40:06 +0100

    IT WORKS

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
 "use strict";
 const bars = document.getElementById("bars");
+const queue = [];
 init();
 
 function init() {
-
-    for(let i=0;i<40;i++) {
-        let bar = document.createElement("div");
-        bar.classList.add("bar");
-        bars.appendChild(bar);
-    }
-
-
-}
-
-function loop() {
-
+    setInterval(modifyModel, 300);
 }
 
 function getNumberOfCustomers() {
-
+    let result = Math.floor(Math.random() * 32);
+    return result;
 }
 
 function displayData() {
+    bars.innerHTML = ''
+    for (let i = 0; i < queue.length; i++) {
+        let bar = document.createElement("div");
+        bar.classList.add("bar");
+        bar.style.height = queue[i] * 100/32 + "%";
+        bars.appendChild(bar);
+    }
 
 }
 
 function modifyModel() {
-
+    let random = getNumberOfCustomers();
+    queue.push(random);
+    if (queue.length > 40) {
+        queue.shift();
+    }
+    displayData();
 }
